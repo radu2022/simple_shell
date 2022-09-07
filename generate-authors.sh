d135af4f82b190f081502900c513f7866320f11a
Amanuel Mekonnen Gebretsadik <amanuelmekonnengebretsadik@gmail.com>
2022-09-07 18:01:23 +0000

    author generation code added.

---
+++ generate-authors.sh
@@ -0,0 +1,9 @@
+et -e
+{
+	cat <<-'EOH'
+	# This file lists all individuals having contributed content to the repository.
+	EOH
+	echo
+	git log --format='%aN <%aE>' | LC_ALL=C.UTF-8 sort -uf
+:wq
+} > AUTHORS
